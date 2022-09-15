d0d069c122cf64ec322a0ec57e80fabff8a93e94
raphaelfiorda <raphaelfiorda@hotmail.com>
2022-09-15 07:23:50 -0300

    refactor: Adiciona mensagens de erro personalizadas nas interfaces

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -2,8 +2,20 @@ import { z } from 'zod';
 import { IVehicle, VehicleZodSchema } from './IVehicle';
 
 export const CarZodSchema = VehicleZodSchema.extend({
-  doorsQty: z.number().min(2).max(4),
-  seatsQty: z.number().min(2).max(7),
+  doorsQty: z
+    .number({
+      required_error: 'Year is required',
+      invalid_type_error: 'Year must be a number',
+    })
+    .min(2, { message: 'Must have at least 2 doors' })
+    .max(4, { message: 'Must have at most 4 doors' }),
+  seatsQty: z
+    .number({
+      required_error: 'Year is required',
+      invalid_type_error: 'Year must be a number',
+    })
+    .min(2, { message: 'Must have at least 2 seats' })
+    .max(7, { message: 'Must have at most 7 seats' }),
 });
 
 export interface ICar extends IVehicle {
--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,11 +1,32 @@
 import { z } from 'zod';
 
 export const VehicleZodSchema = z.object({
-  model: z.string().min(3, { message: 'Must be 3 or more characters long' }),
-  year: z.number().min(1900).max(2022),
-  color: z.string().min(3, { message: 'Must be 3 or more characters long' }),
+  model: z
+    .string({
+      required_error: 'Model is required',
+      invalid_type_error: 'Model must be a string',
+    })
+    .min(3, { message: 'Must be 3 or more characters long' }),
+  year: z
+    .number({
+      required_error: 'Year is required',
+      invalid_type_error: 'Year must be a number',
+    })
+    .min(1900)
+    .max(2022),
+  color: z
+    .string({
+      required_error: 'Model is required',
+      invalid_type_error: 'Model must be a string',
+    })
+    .min(3, { message: 'Must be 3 or more characters long' }),
   status: z.boolean().optional(),
-  buyValue: z.number().int(),
+  buyValue: z
+    .number({
+      required_error: 'Year is required',
+      invalid_type_error: 'Year must be a number',
+    })
+    .int(),
 });
 
 export type IVehicle = z.infer<typeof VehicleZodSchema>;
